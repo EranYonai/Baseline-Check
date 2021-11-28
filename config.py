9fd2b3feb448f60cf58f7f7d1e8fa052ec101916
Eran Yonai <71071063+EranYonai@users.noreply.github.com>
2021-11-28 12:26:02 +0200

    Base writing & initial tests
    - Fixed window resize issue
    - added base find_word_in_db function to check iteration over excel - works.
    - added color_word function to add specific tag colors to a word to be used in pyqt5 gui

--- config.py
+++ config.py
@@ -6,11 +6,17 @@ def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = os.path.expanduser("~/PycharmProjects/OracleWeek")
+        base_path = os.path.expanduser("~BW\\Documents\\GitHub\\Baseline-Check")
     except Exception:
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
-
+# File paths is dumb right now
 FILE_PATHS = {'MAIN_UI': resource_path('dependencies/MainWindow.ui'),
+              'EQP_EXCEL': 'C:\\Users\\BW\\Documents\\GitHub\\Baseline-Check\\bin\\Equipment Traceability.xlsx'
               }
+
+
+EXCEL_SHEET = {'system': 'System',
+               'ws': 'WS',
+               'monitor': 'Monitor'}
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PyQt5 import QtWidgets, uic, QtGui, QtCore, Qt
 import config, sys
+import openpyxl
+
 
 class MainWindow(QtWidgets.QMainWindow):
     """
@@ -12,13 +14,42 @@ class MainWindow(QtWidgets.QMainWindow):
         """
         super(MainWindow, self).__init__()
         uic.loadUi(config.FILE_PATHS['MAIN_UI'], self)
+        self.check_button.clicked.connect(self.find_word_in_db)
 
-    def find_word_in_db(self, word):
-        pass
+    def find_word_in_db(self):
+        word = self.textEdit.toPlainText()
+        try:
+            wb = openpyxl.load_workbook(config.FILE_PATHS['EQP_EXCEL'])  # load excel workbook
+            sheet_system = wb[config.EXCEL_SHEET['system']]  # loads specific sheet, always system until smarter
+            for row in sheet_system.iter_rows():  # iterates all rows (row = tuple)
+                for cell in row:  # iterates cells in row
+                    if cell.value is not None:  # cell.value to get str value.
+                        if word == str(cell.value):  # should actually return int match.
+                            colored_word = color_word(color='green', word=word)
+                            self.textEdit.append(colored_word)
 
-    def color_word(self):
-        pass
-    
+        except Exception as e:
+            print(e)
+
+
+def color_word(color: str, word: str) -> str:
+    """
+    :param color: str = 'green', 'red, 'yellow'.
+    :param word: str of the word you want tagged.
+    :return: string: color_tag + word + close_tag
+    :rtype: str
+    """
+    RED_TAG = "<span style=\" color:#ff0000;\" >"
+    GREEN_TAG = "<span style=\" color:#008000;\" >"
+    YELLOW_TAG = "<span style=\" color:#FFFF00;\" >"
+    CLOSE_TAG = "</span>"
+    if color == 'red':
+        return RED_TAG + word + CLOSE_TAG
+    if color == 'green':
+        return GREEN_TAG + word + CLOSE_TAG
+    if color == 'yellow':
+        return YELLOW_TAG + word + CLOSE_TAG
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
