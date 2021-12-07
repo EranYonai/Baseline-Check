126979735d9ea6a6e28b781d0713b757ffdcdbee
Eran Yonai <71071063+EranYonai@users.noreply.github.com>
2021-12-07 12:29:33 +0200

    Good Progress! see description
    - Some changes to columns names in excel.
    - added teal color to name_to_color in order to show corrections
    - improved complexity and logic! now when find_row_in_db finds an exact match, it saves the row and sends it back in order to specifically look for this row in future searches. row defaults when changing equipment type (e.g. row of WS is saved, moved to U/S -> row defaults)
    - better readability for scan_baseline function
    -----NOTICE THE TODOS IN scan_baseline------
    - find word in db was split into two cases, when row exists and when it's unknown, when unknown, search the column, find exact match, return everything as before but with row. when row is known, search this specific row.
    - Also, the return of find_word_in_db was changed to a dict containing all the information required.
    - in scan_baseline second part (the coloring part), if color is not green (diff is not 1), show correct match from excel in teal color. (add to the string)
    
    That's it pretty much, need to continue check other machines, and do the TODOS!
    - Machines verified:
    * WS -> TODO: need to move the Service Tag to the first or second read value in auto baseline.
    * U/S -> Works!
    System -> Works!
    Pacers -> TODO: need to move the serial number to the first read value in auto baseline.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import config
 import sys
 
 # Global variables
-name_to_color = {'red': '#FF0000', 'yellow': '#FFA500', 'green': '#008000'}
+name_to_color = {'red': '#FF0000', 'yellow': '#FFA500', 'green': '#008000', 'teal': '#74C2E1'}
 Alph_Zero_Val = ord('A') - 1
 
 
@@ -30,6 +30,7 @@ class MainWindow(QtWidgets.QMainWindow):
         Gets a string of a baseline description from GUI and colors the lines according to item's presence DB
         """
         try:
+            row = None
             res = ''
             bl_text = self.textEdit.toPlainText()
             sheet_name = column_name = item_val = None
@@ -40,36 +41,64 @@ class MainWindow(QtWidgets.QMainWindow):
                 elif sheet_name is not None:  # Item Category (sheet) was already found
                     column_name, item_val = line.split(':')
                     item_val = item_val.strip()
+                    #  ---- System special cases ----
                     if sheet_name == 'System':  # Columns in System WS
                         if column_name.startswith('Aquarium'):
                             column_name = 'Aquarium Number'
+                    #  ---- WS special cases ----
                     elif sheet_name == 'WS':  # Columns in Workstation WS
                         pass
+                        #TODO: ---->> if WS validation does funny things, the 'WS Service Tag' should be first, I need to fix this in AutoBaseline export. <<----
+
+                    #  ---- Catheters special cases ----
                     elif sheet_name == 'Catheters':  # Columns in Catheters WS
                         column_name = 'Catalog Number'
+                    #  ---- Pacers special cases ----
                     elif sheet_name == 'Pacers':
                         if column_name.endswith('Serial Number'):
                             column_name = 'Pacer Serial Number'
+                        #TODO: ---->> if WS validation does funny things, the 'Serial Number' should be first, I need to fix this in AutoBaseline export. <<----
+                    #  ---- UltraSound special cases ----
+                    elif sheet_name == "UltraSound":
+                        if column_name.endswith('Ultrasound System'):
+                            column_name = 'Model'
+                    #  ---- Catheters special cases ----
+                    elif sheet_name == "Catheters":
+                        if column_name.startswith('Catheters'):
+                            column_name = 'Model'
                 # Find which sheet to search in
                 elif line.startswith('System #'):
                     sheet_name = 'System'
-                    column_name = item_val = None
+                    column_name = item_val = row = None
+                elif line.startswith('Ultrasound #'):
+                    sheet_name = 'UltraSound'
+                    column_name = item_val = row = None
                 elif line.startswith('Workstation #'):
                     sheet_name = 'WS'
-                    column_name = item_val = None
+                    column_name = item_val = row = None
                 elif line.startswith('Catheters') or line.startswith('Extenders'):
                     sheet_name = 'Catheters'
-                    column_name = item_val = None
+                    column_name = item_val = row = None
                 elif line.startswith('Pacer') or line.startswith('Printer') or line.startswith('EPU'):
                     sheet_name = 'Pacers'
-                    column_name = item_val = None
+                    column_name = item_val = row = None
 
                 # if Sheet, column and item_value have all been detected- Search for similarities in the specified column and sheet
                 if sheet_name and column_name and sheet_name in self.excel_map and column_name in self.excel_map[
                     sheet_name]:
-                    diff = find_word_in_db(sheet_name, self.excel_map[sheet_name][column_name], item_val)
-                    color = 'green' if diff == 1 else 'yellow' if diff >= 0.8 else 'red'
-                    res += f'{color_str(color, line)}<br>'  # Append colored line
+                    if row is None:
+                        answer = find_word_in_db(sheet_name=sheet_name, column=self.excel_map[sheet_name][column_name],
+                                                 word=item_val)
+                    else:
+                        answer = find_word_in_db(sheet_name=sheet_name, column=self.excel_map[sheet_name][column_name],
+                                                 word=item_val, row=row)
+                    color = 'green' if answer['diff'] == 1 else 'yellow' if answer['diff'] >= 0.8 else 'red'
+                    if color == 'green':  # Append colored line
+                        res += f'{color_str(color, line)}<br>'  # Append colored line - green
+                    else:  # Append colored line - if not green, show correction in teal
+                        res += f'{color_str(color, line)} -->> {color_str("teal",answer["match"])}<br>'
+                    print(f'found match in row: {answer["row"]}')
+                    row = answer["row"]
                 else:
                     res += f'{line}<br>'  # append unchecked lines as they are
 
@@ -90,17 +119,30 @@ def color_str(color: str, word: str) -> str:
     return f'<span style="color:{name_to_color[color]};">{word}</span>'
 
 
-def find_word_in_db(sheet_name, column, word):
-    diff = 0
+def find_word_in_db(sheet_name, column, word, row=None):
+    def find_cell_in_db(column, row, word):
+        diff = 0
+        cell_in_excel = str(ws[f'{column}{row}'].value)
+        if cell_in_excel is not None:  # Iterate all rows of specified column
+            print(f'looking for *{word}* in {str(row)}, current cell: *{cell_in_excel}*')
+            diff = max([diff, similar(word, cell_in_excel)])
+            if diff == 1:  # Stop if perfect match was found
+                return {'diff': diff, 'row': row, 'match': cell_in_excel}
+
+        return {'diff': diff, 'row': row, 'match': cell_in_excel}
+
     try:
         wb = openpyxl.load_workbook(config.FILE_PATHS['EQP_EXCEL'])  # load excel workbook
         ws = wb[sheet_name]  # loads specific sheet, always system until smarter
-        for row in range(3, ws.max_row):  # iterates all rows (row = tuple)
-            if ws[f'{column}{row}'].value is not None:  # Iterate all rows of specified column
-                diff = max(diff, similar(word, str(ws[f'{column}{row}'].value)))
-                if diff == 1:  # Stop if perfect match was found
-                    return diff
-        return diff
+        if row is None:
+            for row in range(3, ws.max_row):  # iterates all rows (row = tuple)
+                answer = find_cell_in_db(column=column, row=row, word=word)
+                if answer['diff'] == 1:
+                    print(f"----found exact match for item in row {row}!!----")
+                    return answer
+        else:  # e.g. row is known
+            return find_cell_in_db(column=column, row=row, word=word)
+
     except Exception as e:
         print("Problem finding word in db: " + str(e))
 
